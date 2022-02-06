dbd9957f692ac9c61427bc7fdc9c9267842a1920
Danya_Vid <danilviduykov@gmail.com>
2022-02-06 13:18:38 +0500

    create new react+typescript app

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="app">
+            <header>header</header>
+            <main>main</main>
+            <footer>footer</footer>
+        </div>
+    )
+}
